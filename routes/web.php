a56d31369bf26a18fe713cc479d19e8e52357291
blake <blakelmason@gmail.com>
2020-09-02 09:46:33 -0700

    next button

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', 'SpaController@index')->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
